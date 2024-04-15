4d87eefb5380e15afc168809d3b532e65440598b
Darrin <darrin.lovard.allen@gmail.com>
2024-04-15 01:00:34 -0700

    2nd test

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,12 +3,35 @@ const express = require('express');
 
 const Actions = require('./actions-model');
 const router = express.Router()
-router.get('/',async (req, res) => {
+
+router.get('/', async (req, res) => {
     
   try {
     await Actions.get()
       .then(actions=> {
+  
         res.status(200).json(actions);
+      })
+  }
+  catch(err) {
+    console.log(err)
+  }  
+})
+
+
+
+
+
+router.get('/:id', async (req, res) => {
+    
+  try {
+    await Actions.get(req.params.id)
+      .then(actions=> {
+        if (actions){
+        res.status(200).json(actions);}
+        else {
+          res.status(404).json("Not found")
+        }
       })
   }
   catch(err) {
@@ -16,4 +39,31 @@ router.get('/',async (req, res) => {
   }  
   })
 
-module.exports = router
+  router.post('/', async (req, res) => {
+    
+    try {
+      await Actions.insert()
+        .then(actions=> {
+    
+          res.status(201).json(actions);
+        })
+    }
+    catch(err) {
+      res.status(400).json("trbl")
+    }  
+  })
+  router.delete('/:id', async  (req, res) => {
+    try {await Actions.remove(req.params.id)
+      .then(count => {
+        if (count > 0) {
+          res.status(200).json({ message: 'The act has been nuked' });
+        } else {
+          res.status(404).json({ message: 'The act could not be found' });
+        }
+   })}
+   catch(err) {
+      res.status(404).json("trouble")
+   }
+  })
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -14,5 +14,42 @@ router.get('/', async  (req, res) => {
    }
     
   })
-
+  router.get('/:id', async  (req, res) => {
+    try {await Project.get(req.params.id)
+      .then(proj=> {
+        if (proj){
+        res.status(200).json(proj)}
+        else {
+          res.status(404).json("not found")
+        }
+   })}
+   catch(err) {
+      console.log(err)
+   }
+    
+  })
+  router.post('/', async  (req, res) => {
+    try {await Project.insert(req.body)
+      .then(proj=> {
+        res.status(201).json(proj)
+   })}
+   catch(err) {
+      res.status(400).json("trouble")
+   }
+    
+  })
+  router.delete('/:id', async  (req, res) => {
+    try {await Project.remove(req.params.id)
+      .then(count => {
+        if (count > 0) {
+          res.status(200).json({ message: 'The projhas been nuked' });
+        } else {
+          res.status(404).json({ message: 'The proj could not be found' });
+        }
+   })}
+   catch(err) {
+      res.status(404).json("trouble")
+   }
+    
+  })
 module.exports = router
